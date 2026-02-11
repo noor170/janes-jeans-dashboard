a120a07d25d2037409ddb344227116b9cba6df1d
noor <nooralam52stamford@gmail.com>
2026-02-11 19:15:54 +0600

    test: fix AuthControllerTest role type and disable Liquibase for tests

--- backend/src/test/java/com/janesjeans/api/controller/AuthControllerTest.java
+++ backend/src/test/java/com/janesjeans/api/controller/AuthControllerTest.java
@@ -20,7 +20,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.liquibase.enabled=false")
 @AutoConfigureMockMvc
 class AuthControllerTest {
 
